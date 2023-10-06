94f4a4fdd6c0921e54148a0cfe175f4733d402e5
Frank van de Voorde <daemonaz@kymp.net>
2023-10-06 15:41:44 +0300

    Initial Commit

---
+++ database/migrations/2023_10_06_114218_create_logged_messages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('logged_messages', function (Blueprint $table) {
+            $table->id();
+            $table->enum('event_name', ['update', 'create', 'delete']);
+            $table->binary('original_data')->nullable();
+            $table->binary('new_data')->nullable();
+            $table->binary('user_data');
+            $table->string('route');
+            $table->integer('user_id');
+            $table->string('ip_address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('logged_messages');
+    }
+};
