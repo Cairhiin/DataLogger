33bc8ad435a28545790847b6ad51f7629d646ffd
Frank van de Voorde <daemonaz@kymp.net>
2023-10-13 16:04:27 +0300

    Fix a minor color bug in the light version

--- resources/js/Components/Events/EventDetails.vue
+++ resources/js/Components/Events/EventDetails.vue
@@ -1,35 +1,35 @@
 <template>
     <div class="p-4">
         <div class="flex items-center">
-            <div class="w-32 text-sm font-bold dark:text-zinc-400 text-zinc-700 uppercase">ID</div>
+            <div class="w-32 text-sm font-bold dark:text-zinc-400 text-zinc-500 uppercase">ID</div>
             <div class="text-zinc-900 dark:text-zinc-100">{{ event.id }}</div>
         </div>
         <div class="flex items-center">
-            <div class="w-32 text-sm font-bold dark:text-zinc-400 text-zinc-700 uppercase">Datum:</div>
+            <div class="w-32 text-sm font-bold dark:text-zinc-400 text-zinc-500 uppercase">Date:</div>
             <div class="text-zinc-900 dark:text-zinc-100">{{ formatDate(event.created_at) }}</div>
         </div>
         <div class="flex items-center">
-            <div class="w-32 text-sm font-bold dark:text-zinc-400 text-zinc-700 uppercase">IP Adres:</div>
+            <div class="w-32 text-sm font-bold dark:text-zinc-400 text-zinc-500 uppercase">IP Address:</div>
             <div class="text-zinc-900 dark:text-zinc-100">{{ event.ip_address }}</div>
         </div>
         <div class="flex items-center">
-            <div class="w-32 text-sm font-bold dark:text-zinc-400 text-zinc-700 uppercase">Data:</div>
+            <div class="w-32 text-sm font-bold dark:text-zinc-400 text-zinc-500 uppercase">Data:</div>
             <div class="text-zinc-900 dark:text-zinc-100">{{ event.model }}</div>
         </div>
         <div class="flex items-center">
-            <div class="w-32 text-sm font-bold dark:text-zinc-400 text-zinc-700 uppercase">Route:</div>
+            <div class="w-32 text-sm font-bold dark:text-zinc-400 text-zinc-500 uppercase">Route:</div>
             <div class="text-zinc-900 dark:text-zinc-100">{{ event.route }}</div>
         </div>
         <div class="flex items-center">
-            <div class="w-32 text-sm font-bold dark:text-zinc-400 text-zinc-700 uppercase">Type:</div>
+            <div class="w-32 text-sm font-bold dark:text-zinc-400 text-zinc-500 uppercase">Type:</div>
             <div class="text-zinc-900 dark:text-zinc-100">{{ event.event_type }}</div>
         </div>
         <div class="grid grid-cols-5 items-center">
-            <div class="text-sm font-bold dark:text-zinc-400 text-zinc-700 uppercase">Nieuwe Data:</div>
+            <div class="text-sm font-bold dark:text-zinc-400 text-zinc-500 uppercase">Updated Data:</div>
             <div class="col-span-4 text-zinc-900 dark:text-zinc-100 truncate">{{ formatData(event.new_data) }}</div>
         </div>
         <div class="grid grid-cols-5 items-center">
-            <div class="text-sm font-bold dark:text-zinc-400 text-zinc-700 uppercase">Oude Data:</div>
+            <div class="text-sm font-bold dark:text-zinc-400 text-zinc-500 uppercase">Old Data:</div>
             <div class="col-span-4 text-zinc-900 dark:text-zinc-100 truncate">{{ formatData(event.original_data) }}</div>
         </div>
     </div>
