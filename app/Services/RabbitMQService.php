d87e6b60947c10c3257cba0e5be23bffc9a02cc9
Frank van de Voorde <daemonaz@kymp.net>
2023-10-14 21:47:16 +0300

    Change to a durable RabbitMQ Queue

--- app/Services/RabbitMQService.php
+++ app/Services/RabbitMQService.php
@@ -14,7 +14,7 @@ class RabbitMQService
         $connection = new AMQPStreamConnection(env('MQ_HOST'), env('MQ_PORT'), env('MQ_USER'), env('MQ_PASS'), env('MQ_VHOST'));
         $channel = $connection->channel();
         $channel->exchange_declare(env('MQ_EXCHANGE'), 'direct', false, false, false);
-        $channel->queue_declare(env('MQ_QUEUE'), false, false, false, false);
+        $channel->queue_declare(env('MQ_QUEUE'), false, true, false, false);
         $channel->queue_bind(env('MQ_QUEUE'), env('MQ_EXCHANGE'), $key);
         $msg = new AMQPMessage($message);
         $channel->basic_publish($msg, env('MQ_EXCHANGE'), $key);
@@ -40,7 +40,7 @@ class RabbitMQService
             }
         };
 
-        $channel->queue_declare(env('MQ_QUEUE'), false, false, false, false);
+        $channel->queue_declare(env('MQ_QUEUE'), false, true, false, false);
         $channel->basic_consume(env('MQ_QUEUE'), '', false, true, false, false, $callback);
         echo 'Waiting for new message on ' . env('MQ_QUEUE'), " \n";
         while ($channel->is_consuming()) {
