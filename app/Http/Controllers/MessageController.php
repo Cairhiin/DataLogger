47346951c51951fb9237f3e1f8f3b3d2cd5158a8
Frank van de Voorde <daemonaz@kymp.net>
2023-10-14 19:22:09 +0300

    Add pagination to message controller - needs refactoring

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -8,6 +8,8 @@ use App\Utilities\Encryption;
 
 class MessageController extends Controller
 {
+    const PAGINATE = 15;
+
     public function store(Request $request)
     {
         $enc_key = "";
@@ -51,6 +53,8 @@ class MessageController extends Controller
         $user = Auth()->user();
         $enc_key = $user->encryption_key;
         $hasAccess = false;
+        $page = $request->page == 0 || $request->page == "undefined" ? 1 : $request->page;
+        $lineStart = ($page - 1) * self::PAGINATE;
 
         if ($user->role->name != "member") {
             $hasAccess = true;
@@ -58,8 +62,20 @@ class MessageController extends Controller
 
         $logFile = file(storage_path() . '/logs/user-data.log');
         $messageCollection = [];
+        $links = [];
+        $numberOfLinks = 1;
+        $numberOfResults = 0;
+        $prev = $page == 1 ? null : '/event/messages?page=' . $page - 1;
 
-        foreach ($logFile as $line) {
+        $links[] = array("url" => $prev, "label" => "previous");
+        $links[] = array("url" => "/event/messages?page=" . 1, "label" => 1);
+
+        foreach ($logFile as $line_num => $line) {
+            if (($line_num + 1) % 15 == 0) {
+                $numberOfLinks++;
+                $links[] = array("url" => "/event/messages?page=" . $numberOfLinks, "label" => $numberOfLinks);
+            }
+
             // Remove the first part of the string to get the logged data
             $s = explode(": ", $line);
             $s = $s[1];
@@ -73,17 +89,32 @@ class MessageController extends Controller
                 continue;
             }
 
+            // Increase number of results for pagination
+            $numberOfResults++;
+
+
+            if ($line_num < $lineStart || $line_num >= $lineStart + self::PAGINATE) {
+                continue;
+            }
+
             // Remove the email from the data that is being sent to the frontend for privacy reasons
             unset($content['user_email']);
 
             // Decrypt App identifier and IP
             $content["app_id"] = Encryption::decryptUsingKey($enc_key, $content["app_id"]);
             $content["ip_address"] = Encryption::decryptUsingKey($enc_key, $content["ip_address"]);
-            $messageCollection[] = array('date' => $date, 'content' => $content);
+            $content["created_at"] = $date;
+            $content["id"] = $line_num;
+
+            $messageCollection[] = $content;
         }
+
+        $next = $page  * 15 >= $numberOfResults ? null : '/event/messages?page=' . $page + 1;
+        $links[] = array("url" => $next, "label" => "next");
 
         return Inertia::render('Event/Messages', [
             'messages' => $messageCollection,
+            'links' => $links
         ]);
     }
 }
--- resources/js/Pages/Event/Messages.vue
+++ resources/js/Pages/Event/Messages.vue
@@ -1,15 +1,54 @@
 <template>
-    {{ messages }}
+    <app-layout>
+        <template #header>
+            <h2>
+                URL Events
+            </h2>
+        </template>
+        <div>
+            <div class="grid grid-cols-5 items-center gap-8 p-2 font-bold rounded-t
+            bg-zinc-900 text-zinc-100 dark:bg-zinc-100 dark:text-zinc-900">
+                <div class="col-span-2">Date</div>
+                <div>Data</div>
+                <div>Route</div>
+                <div>Application ID</div>
+            </div>
+            <event-list :events="messages" @show-details="showDetails" />
+        </div>
+        <pagination :links="links" />
+    </app-layout>
 </template>
 <script>
+import Pagination from '@/Components/Custom/Pagination.vue';
+import EventList from '@/Components/Events/EventList.vue';
+import AppLayout from '@/Layouts/AppLayout.vue';
+
+
 export default {
     Data() {
         return {
 
         }
     },
+    components: {
+        EventList,
+        AppLayout,
+        Pagination
+    },
     props: {
-        messages: Object
+        messages: Array,
+        links: Array
+    },
+    methods: {
+        showDetails(id) {
+            this.selectedId = id;
+            this.modalIsShowing = true;
+            const log = this.getSelectedLog();
+            this.encrypted = {
+                original_data: log.original_data,
+                new_data: log.new_data
+            };
+        },
     }
 }
 </script>
