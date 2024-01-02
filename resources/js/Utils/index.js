af74895d4a3d69913ed70c3214ec2fa19cf7f7b0
Frank van de Voorde <daemonaz@kymp.net>
2024-01-02 16:13:22 +0200

    Fix the date format to include leading 0s

--- resources/js/Utils/index.js
+++ resources/js/Utils/index.js
@@ -1,10 +1,12 @@
 export function formatDate(d, type) {
     const time = new Date(new Date(d).getTime());
-    const date = new Date(d).toLocaleDateString("nl-NL", {
-        day: "numeric",
-        month: "numeric",
-        year: "numeric",
-    });
+    const date = new Date(d);
+
+    let day = date.getDate();
+    if (day < 10) day = `0${day}`;
+
+    let month = date.getMonth() + 1;
+    if (month < 10) month = `0${month}`;
 
     const hours =
         time.getHours() < 10 ? `0${time.getHours()}` : time.getHours();
@@ -14,7 +16,7 @@ export function formatDate(d, type) {
         time.getSeconds() < 10 ? `0${time.getSeconds()}` : time.getSeconds();
 
     if (type === "time") return `${hours}:${minutes}:${seconds}`;
-    if (type === "date") return date;
+    if (type === "date") return `${day}-${month}-${date.getFullYear()}`;
 
-    return `${date} (${hours}:${minutes}:${seconds})`;
+    return `${day}-${month}-${date.getFullYear()} (${hours}:${minutes}:${seconds})`;
 }
